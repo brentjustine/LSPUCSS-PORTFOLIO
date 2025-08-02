d0ee1dc92e199d8c7d64ac9c6cd4738b61b911a4
Brent Justine Barbadillo <168087779+brentjustine@users.noreply.github.com>
2025-08-02 18:59:23 +0800

    Update supabaseClient.ts

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jvqguqwpbcsdxjbfutxw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2cWd1cXdwYmNzZHhqYmZ1dHh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NjQ0OTgsImV4cCI6MjA2OTU0MDQ5OH0.lL7du3C9MojGJxRwV6lQqLXcF1JfL8KJS5-8JRWnDfI'; // Your full key
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+export { supabase };
